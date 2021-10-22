0e01925fd5a7928b0098783526f6837e4e158950
Jesus Cepeda <jcepedaf@gmail.com>
2021-10-22 21:57:40 +0200

    agrego cambios a homework de node 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Dashboard from './components/Dashboard/Dashboard';
-import Preferences from './components/Preferences/Preferences';
+import Home from './components/Home/Home';
+import Singup from './components/Singup/Singup';
 import Login from './components/Login/Login';
 
 function App() {
   const [token, setToken] = useState();
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
+  if(token) {
+    return (
+      <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/singin">
+            <Login setToken={setToken} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+    )
+  };
 
   return (
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
         <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
+        <Route path="/singin">
+            <Login setToken={setToken} />
           </Route>
-          <Route path="/preferences">
-            <Preferences />
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/singup">
+            <Singup />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ src/components/Singup/Singup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Singup() {
+  return(
+    <h2>Singup</h2>
+  );
+}
