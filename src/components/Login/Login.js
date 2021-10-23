afc722191d1f27d9f83cc21621dabfe43b180221
Jesus Cepeda <jcepedaf@gmail.com>
2021-10-23 12:31:43 +0200

    agrego cambios a homework de node 3

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './Login.css';
 
 export default function Login() {
+
+  var url = 'http://localhost:4000/user/sign-in';
+  var data = {"email":"pedro4@gmail.com","password":"123456" };
+  
+  fetch(url, {
+    method: 'POST', 
+    body: JSON.stringify(data), 
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => console.log('Success:', response));
+
   return(
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
+      <form action="http://localhost:4000/user/sign-in" method="POST">
         <label>
           <p>email</p>
           <input type="text" />
--- src/components/Singup/Singup.js
+++ src/components/Singup/Singup.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Singup() {
-  return(
-    <h2>Singup</h2>
+const [name,setName] = useState("");
+const [email,setEmail] = useState("");
+const [password,setPassword] = useState("");
 
+  const singUp2 = ()=>{ 
+  var url = 'http://localhost:4000/user/sign-up';
+  var data = {"name":name,"email":email,"password":password };
+  console.log(data)
+  fetch(url, {
+    method: 'POST', 
+    body: JSON.stringify(data), 
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+   .then(response => console.log('Success:', response))
+   .catch(error => console.error('Error:', error));
+
+
+ }
+
+  return(
     <div className="login-wrapper">
-      <h1>please fill in your data</h1>
-      <form>
+     <h1>please fill in your data</h1>
+      <form >
         <label>
           <p>Name</p>
-          <input type="text" />
+          <input type="text" onChange={(event)=>setName(event.target.value)}/>
         </label>
         <label>
           <p>email</p>
-          <input type="text" />
+          <input type="text" onChange={(event)=>setEmail(event.target.value)}/>
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input type="password" onChange={(event)=>setPassword(event.target.value)}/>
         </label>
         <div>
-          <button type="submit">Submit</button>
+          <button onClick={singUp2}>Submit</button>
         </div>
       </form>
     </div>
